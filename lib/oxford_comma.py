13ee53cb8387a514033f8d599eb22a6c4f956e85
Martin <mmaina290@gmail.com>
2024-01-09 21:52:09 +0300

    join

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    '''converts a list into a string using the Oxford Comma'''
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return ' and '.join(items)
+    else:
+        return ', '.join(items[:-1]) + ', and ' + items[-1]
+
